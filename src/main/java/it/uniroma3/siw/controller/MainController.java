27b6fcc5742ef236ecd82ec0e07182e472c0d362
GabryDX <gabrydx@hotmail.com>
2017-06-09 10:12:48 +0200

    Aggiunto MainController

---
+++ src/main/java/it/uniroma3/siw/controller/MainController.java
@@ -0,0 +1,24 @@
+package it.uniroma3.siw.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// controller to access the login page
+@Controller
+public class MainController {
+
+	// Login form
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+	// Login form with error
+	@RequestMapping("/login-error.html")
+	public String loginError(Model model) {
+		model.addAttribute("loginError", true);
+		return "login";
+	}
+
+}
