c2aeed7a42c2eab798749acf9f4ed2d9902e5f4f
FedeM9 <moscatelli.federico@live.it>
2017-06-09 10:34:50 +0200

    aggiornato opera controller

--- src/main/java/it/uniroma3/siw/controller/OperaController.java
+++ src/main/java/it/uniroma3/siw/controller/OperaController.java
@@ -1,12 +1,39 @@
 package it.uniroma3.siw.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import it.uniroma3.siw.modello.Opera;
 import it.uniroma3.siw.service.OperaService;
 
 @Controller
 public class OperaController {
+	
 	@Autowired
-	OperaService service;
+	OperaService operaService;
+	
+    @GetMapping("/opera")
+    public String showForm(Opera opera) {
+        return "form";
+    }
+
+    @PostMapping("/opera")
+    public String checkCustomerInfo(@Valid @ModelAttribute Opera opera, 
+    									BindingResult bindingResult, Model model) {
+    	
+        if (bindingResult.hasErrors()) {
+            return "form";
+        } else {
+        	model.addAttribute(opera);
+        	operaService.add(opera); 
+        }
+        return "results";
+    }
 }
