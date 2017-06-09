0da61f8e5fe627b33801480a62c87572dee4ef1f
GabryDX <gabrydx@hotmail.com>
2017-06-09 09:52:46 +0200

    Modifica Utente

--- src/main/java/it/uniroma3/siw/modello/Utente.java
+++ src/main/java/it/uniroma3/siw/modello/Utente.java
@@ -1,5 +1,33 @@
 package it.uniroma3.siw.modello;
 
 public class Utente {
+	
+	private String username;
+	private String password;
 
+	public Utente() {
+	}
+
+	public Utente(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 }
