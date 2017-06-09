cc93158accab0ed2ad876b29fde9897790763edc
GabryDX <gabrydx@hotmail.com>
2017-06-09 10:20:08 +0200

    Modifiche Service e Controller

--- src/main/java/it/uniroma3/siw/service/OperaService.java
+++ src/main/java/it/uniroma3/siw/service/OperaService.java
@@ -1,12 +1,12 @@
 package it.uniroma3.siw.service;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import it.uniroma3.siw.modello.Opera;
 import it.uniroma3.siw.repository.OperaRepository;
-
-
 
 @Service
 public class OperaService {
@@ -17,5 +17,14 @@ public class OperaService {
     public Iterable<Opera> findAll() {
         return this.operaRepository.findAll();
     }
+    
+    @Transactional
+    public void add(final Opera opera) {
+        this.operaRepository.save(opera);
+    }
+
+	public Opera findbyId(Long id) {
+		return this.operaRepository.findOne(id);
+	}
 	
 }
