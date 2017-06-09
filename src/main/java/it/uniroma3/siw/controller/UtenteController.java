fc007e90d5fbec9991f81fb3c35b622bfcebe1dc
FedeM9 <moscatelli.federico@live.it>
2017-06-09 11:58:30 +0200

    Login Utente

---
+++ src/main/java/it/uniroma3/siw/controller/UtenteController.java
@@ -0,0 +1,29 @@
+package it.uniroma3.siw.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import it.uniroma3.siw.modello.Utente;
+import it.uniroma3.siw.service.UtenteService;
+
+@Controller
+public class UtenteController {
+	@Autowired
+	UtenteService utenteService;
+	
+	@PostMapping("/utenteLogin")
+    public String checkCustomerInfo(@Valid @ModelAttribute Utente utente, 
+    									BindingResult bindingResult, Model model) {
+    	
+        if (bindingResult.hasErrors()) {
+            return "Error"; //in caso di errore 
+        }
+        return "LoginUtenteEffettuato";
+    }
+}
