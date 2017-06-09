ea42b629ae723201f6e1821a40abda8a3e242b66
GabryDX <gabrydx@hotmail.com>
2017-06-09 09:56:20 +0200

    Modifica Admin

--- src/main/java/it/uniroma3/siw/modello/Admin.java
+++ src/main/java/it/uniroma3/siw/modello/Admin.java
@@ -1,9 +1,14 @@
 package it.uniroma3.siw.modello;
 
 public class Admin extends Utente {
-
+	
 	public Admin(Long id) {
 		super(id);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Admin(Long id, String username, String password) {
+		super(id, username, password);
 		// TODO Auto-generated constructor stub
 	}
 
