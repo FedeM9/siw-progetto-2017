5ad85014d428d8e31344355555ec1ca40f7089b1
FedeM9 <moscatelli.federico@live.it>
2017-06-09 10:21:04 +0200

    Aggiunte Operazioni su OperaRepository

--- src/main/java/it/uniroma3/siw/repository/OperaRepository.java
+++ src/main/java/it/uniroma3/siw/repository/OperaRepository.java
@@ -1,12 +1,17 @@
 package it.uniroma3.siw.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+
+import it.uniroma3.siw.modello.Autore;
 import it.uniroma3.siw.modello.Opera;
 
 public interface OperaRepository extends CrudRepository<Opera, Long>{
 	
 	  List<Opera> findAll(String name);
-
+	  List<Opera> findByAutore(Autore autore);
+	  List<Opera> findByAnno(Date  anno);
+	  List<Opera> findByTecnica(String tecnica);
 }
