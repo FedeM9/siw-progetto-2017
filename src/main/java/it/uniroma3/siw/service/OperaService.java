12452c53ea3256184cf83da2d6e923d3cafb95d3
FedeM9 <moscatelli.federico@live.it>
2017-06-09 10:29:08 +0200

    Aggiunti servizi di Opera

--- src/main/java/it/uniroma3/siw/service/OperaService.java
+++ src/main/java/it/uniroma3/siw/service/OperaService.java
@@ -1,8 +1,12 @@
 package it.uniroma3.siw.service;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import it.uniroma3.siw.modello.Autore;
 import it.uniroma3.siw.modello.Opera;
 import it.uniroma3.siw.repository.OperaRepository;
 
@@ -17,5 +21,18 @@ public class OperaService {
     public Iterable<Opera> findAll() {
         return this.operaRepository.findAll();
     }
+	public   Iterable<Opera> findByAutore(Autore autore){
+		return this.operaRepository.findByAutore(autore);
+	}
 	
+	public Iterable<Opera> findByAnno(Date  anno){
+		return this.operaRepository.findByAnno(anno);
+	}
+	public Iterable<Opera> findByTecnica(String tecnica){
+		return this.operaRepository.findByTecnica(tecnica);
+	}
+	
+	public void add(final Opera opera){
+		this.operaRepository.save(opera);
+	}
 }
