db68fa41d2694052321ec4fbbf6bd8c2536013e9
GabryDX <gabrydx@hotmail.com>
2017-06-09 09:59:53 +0200

    Aggiunta tag a Utente

--- src/main/java/it/uniroma3/siw/modello/Utente.java
+++ src/main/java/it/uniroma3/siw/modello/Utente.java
@@ -1,9 +1,25 @@
 package it.uniroma3.siw.modello;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Utente {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotNull
+	@Size(min=1)
 	private String username;
+	
+	@NotNull
+	@Size(min=1)
 	private String password;
 
 	public Utente(Long id) {
