d104b86ac975d32d431395485fe9c9aedc350b52
FedeM9 <moscatelli.federico@live.it>
2017-06-11 15:17:09 +0200

    implementata

--- src/main/java/it/uniroma3/siw/modello/Admin.java
+++ src/main/java/it/uniroma3/siw/modello/Admin.java
@@ -1,18 +1,59 @@
 package it.uniroma3.siw.modello;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
 
 @Entity
-public class Admin extends Utente {
+public class Admin {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+
+	@Size(min=1)
+	private String username;
+	
+
+	@Size(min=1)
+	private String password;
+
+	public Admin(){
+		
+	}
 	
 	public Admin(Long id) {
-		super(id);
-		// TODO Auto-generated constructor stub
+		this.id = id;
 	}
 
 	public Admin(Long id, String username, String password) {
-		super(id, username, password);
-		// TODO Auto-generated constructor stub
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 }
